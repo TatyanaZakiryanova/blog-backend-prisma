4038db61fb149f259bcdbd3cfadb72961e31d3dd
TatyanaZakiryanova <elenastrell@yandex.ru>
2025-06-15 18:17:20 +0300

    fix cookie settings

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -16,7 +16,7 @@ export const register = async (
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -36,7 +36,7 @@ export const login = async (req: Request<{}, {}, LoginDto>, res: Response, next:
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
